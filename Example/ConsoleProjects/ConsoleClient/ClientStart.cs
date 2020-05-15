33c1278aed12da797c893792d81f9021c306a77c
PlaneZhong <1785275942@qq.com>
2020-05-15 22:46:11 +0800

    增加数据压缩；网络容错

--- Example/ConsoleProjects/ConsoleClient/ClientStart.cs
+++ Example/ConsoleProjects/ConsoleClient/ClientStart.cs
@@ -7,13 +7,29 @@ namespace ConsoleClient {
         static PESocket<ClientSession, NetMsg> client;
 
         static void Main(string[] args) {
+            //TestCode
+            //Random rd = new Random();
+            //byte[] data = new byte[10];
+            //rd.NextBytes(data);
+            //byte[] compressBytes = PETool.Compress(data);
+            //byte[] deCompress = PETool.DeCompress(compressBytes);
+
+            //for(int i = 0; i < data.Length; i++) {
+            //    if(data[i] != deCompress[i]) {
+            //        Console.WriteLine("压缩数据出错。");
+            //        return;
+            //    }
+            //}
+            //Console.WriteLine("压缩数据成功。");
+
+
             client = new PESocket<ClientSession, NetMsg>();
             client.StartAsClient(IPCfg.srvIP, IPCfg.srvPort);
 
             Console.WriteLine("\nInput 'quit' to stop client!");
-            while (true) {
+            while(true) {
                 string ipt = Console.ReadLine();
-                if (ipt == "quit") {
+                if(ipt == "quit") {
                     client.Close();
                     break;
                 }
--- PESocket/PETool.cs
+++ PESocket/PETool.cs
@@ -8,6 +8,8 @@
 
 using System;
 using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace PENet {
@@ -29,20 +31,60 @@ namespace PENet {
             return pkg;
         }
 
-        public static byte[] Serialize<T>(T pkg) where T : PEMsg {
-            using (MemoryStream ms = new MemoryStream()) {
-                BinaryFormatter bf = new BinaryFormatter();
-                bf.Serialize(ms, pkg);
-                ms.Seek(0, SeekOrigin.Begin);
-                return ms.ToArray();
+        public static byte[] Serialize<T>(T msg) where T : PEMsg {
+            using(MemoryStream ms = new MemoryStream()) {
+                try {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.Serialize(ms, msg);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    return Compress(ms.ToArray());
+                }
+                catch(SerializationException e) {
+                    LogMsg("Failed to serialize. Reason: " + e.Message, LogLevel.Error);
+                    return null;
+                }
             }
         }
 
-        public static T DeSerialize<T>(byte[] bs) where T : PEMsg {
-            using (MemoryStream ms = new MemoryStream(bs)) {
-                BinaryFormatter bf = new BinaryFormatter();
-                T pkg = (T)bf.Deserialize(ms);
-                return pkg;
+        public static T DeSerialize<T>(byte[] bytes) where T : PEMsg {
+            using(MemoryStream ms = new MemoryStream(DeCompress(bytes))) {
+                try {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    T msg = (T)bf.Deserialize(ms);
+                    return msg;
+                }
+                catch(SerializationException e) {
+                    LogMsg("Failed to deserialize. Reason: " + e.Message + " bytesLen:" + bytes.Length, LogLevel.Error);
+                    return null;
+                }
+            }
+        }
+
+        public static byte[] Compress(byte[] input) {
+            using(MemoryStream outMS = new MemoryStream()) {
+                using(GZipStream gzs = new GZipStream(outMS, CompressionMode.Compress, true)) {
+                    gzs.Write(input, 0, input.Length);
+                    gzs.Close();
+                    return outMS.ToArray();
+                }
+            }
+        }
+
+        public static byte[] DeCompress(byte[] input) {
+            using(MemoryStream inputMS = new MemoryStream(input)) {
+                using(MemoryStream outMS = new MemoryStream()) {
+                    using(GZipStream gzs = new GZipStream(inputMS, CompressionMode.Decompress)) {
+                        byte[] bytes = new byte[1024];
+                        int len = 0;
+                        while((len = gzs.Read(bytes, 0, bytes.Length)) > 0) {
+                            outMS.Write(bytes, 0, len);
+                        }
+                        //高版本可用
+                        //gzs.CopyTo(outMS);
+                        gzs.Close();
+                        return outMS.ToArray();
+                    }
+                }
             }
         }
 
@@ -50,31 +92,31 @@ namespace PENet {
         public static bool log = true;
         public static Action<string, int> logCB = null;
         public static void LogMsg(string msg, LogLevel lv = LogLevel.None) {
-            if (log != true) {
+            if(log != true) {
                 return;
             }
             //Add Time Stamp
             msg = DateTime.Now.ToLongTimeString() + " >> " + msg;
-            if (logCB != null) {
+            if(logCB != null) {
                 logCB(msg, (int)lv);
             }
             else {
-                if (lv == LogLevel.None) {
+                if(lv == LogLevel.None) {
                     Console.WriteLine(msg);
                 }
-                else if (lv == LogLevel.Warn) {
+                else if(lv == LogLevel.Warn) {
                     //Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("//--------------------Warn--------------------//");
                     Console.WriteLine(msg);
                     //Console.ForegroundColor = ConsoleColor.Gray;
                 }
-                else if (lv == LogLevel.Error) {
+                else if(lv == LogLevel.Error) {
                     //Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("//--------------------Error--------------------//");
                     Console.WriteLine(msg);
                     //Console.ForegroundColor = ConsoleColor.Gray;
                 }
-                else if (lv == LogLevel.Info) {
+                else if(lv == LogLevel.Info) {
                     //Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("//--------------------Info--------------------//");
                     Console.WriteLine(msg);
